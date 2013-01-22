1649d1ab97e292beef8639fab1fef66a3511ca6d
gawde <gawderprasad@gmail.com>
2013-01-22 13:25:56 +0530

    Added File

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pmd::Application.config.secret_token = '014eba987e35075c9015b978f4c51a795387e37478b6410d8b26f62656c4595700ab8497d00a80c962b30ee7eaa6f98f395b3638dd2a7324090cc23ebac546a9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Pmd::Application.config.session_store :cookie_store, key: '_pmd_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Pmd::Application.config.session_store :active_record_store
